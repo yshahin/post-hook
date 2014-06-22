65636cfcab7e33a3ff5aa937ec7011dcc2edb2e7
Youssef Shahin <yshahin@gmail.com>
2014-06-22 21:13:54 +0300

    testing

--- hook.rb
+++ hook.rb
@@ -6,7 +6,7 @@ end
 
 post '/' do
   puts params.inspect
-  if params[:commits][0][:branch] == ENV['branch']
+  if params['data']['commits'][0]['branch'] == ENV['branch']
     uri = URI("https://www.codeship.io/hook/ENV['project_uuid']")
     Net::HTTP.post_form(uri, params)
   else
