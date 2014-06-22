984070f40042c0b67814cdbbb40eacb40af7eec5
Youssef Shahin <yshahin@gmail.com>
2014-06-22 23:18:48 +0300

    read data correctly

--- hook.rb
+++ hook.rb
@@ -1,20 +1,14 @@
 require 'sinatra'
-require "logger"
-
-configure do
-  LOG = Logger.new(STDOUT)
-  LOG.level = Logger.const_get ENV['LOG_LEVEL'] || 'DEBUG'
-
-  LOG.info 'I am logging something.'
-end
+require 'json'
 
 get '/' do
   "Nothing Here - Go Home"
 end
 
 post '/' do
-  LOG.debug params
-
-  uri = URI("https://www.codeship.io/hook/ENV['project_uuid']")
-  Net::HTTP.post_form(uri, params)
+  data = JSON.parse(params['payload'])
+  if data['commits'].first['branch'] == ENV['branch']
+    uri = URI("https://www.codeship.io/hook/ENV['project_uuid']")
+    Net::HTTP.post_form(uri, params)
+  end
 end
