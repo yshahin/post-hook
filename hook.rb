8a66477c6fb296873528cc7c32f3630bf5489dae
Youssef Shahin <yshahin@gmail.com>
2014-06-25 21:35:05 +0300

    syntax error: string interpolation

--- hook.rb
+++ hook.rb
@@ -8,7 +8,7 @@ end
 post '/' do
   data = JSON.parse(params['payload'])
   if data['commits'].first['branch'] == ENV['branch']
-    uri = URI("https://www.codeship.io/hook/ENV['project_uuid']")
+    uri = URI("https://www.codeship.io/hook/#{ENV['project_uuid']}")
     Net::HTTP.post_form(uri, params)
   end
 end
