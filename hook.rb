561f262624e31b34275382321385a18659e7c013
Youssef Shahin <yshahin@gmail.com>
2014-06-28 18:36:42 +0200

    supports multiple branch names

--- hook.rb
+++ hook.rb
@@ -7,7 +7,7 @@ end
 
 post '/' do
   data = JSON.parse(params['payload'])
-  if data['commits'].first['branch'] == ENV['branch']
+  if ENV['branch'].split(',').include? data['commits'].first['branch']
     uri = URI("https://www.codeship.io/hook/#{ENV['project_uuid']}")
     Net::HTTP.post_form(uri, params)
   end
